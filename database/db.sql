ca944caa414e5f0e13e1a6677b1add0f9fafad84
elotrodiego <elotrodiegoluna@gmail.com>
2022-05-22 03:25:25 -0400

    bd

--- database/db.sql
+++ database/db.sql
@@ -3,13 +3,14 @@ CREATE DATABASE musicpro;
 USE musicpro;
 
 CREATE TABLE usuario(
-    id INT(5) AUTO_INCREMENT PRIMARY KEY,
+    id_user INT(5) AUTO_INCREMENT,
     nombre VARCHAR(128) NOT NULL,
     apellido VARCHAR(128) NOT NULL,
     rut VARCHAR(20) NOT NULL,
-    correo VARCHAR(128) NOT NULL,
-    contraseña VARCHAR(128) NOT NULL,
-    tipo_permiso CHAR(1) NOT NULL  
+    email VARCHAR(128) NOT NULL,
+    contrasena VARCHAR(128) NOT NULL,
+    tipo_permiso CHAR(1) NOT NULL,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE producto(
@@ -17,10 +18,10 @@ CREATE TABLE producto(
     nombre VARCHAR(128) NOT NULL,
     codigo INT(8) NOT NULL,
     valor INT(8) NOT NULL,
-    foto BLOB NOT NULL,
+    foto VARCHAR(600) NOT NULL,
     oferta INT(5) NOT NULL,
     categoria CHAR(1) NOT NULL,
-    descripcion VARCHAR(150),
+    descripcion VARCHAR(300),
     PRIMARY KEY(id)
 );
 
